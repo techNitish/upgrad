524bb1f2b94b71e81318269bb9ccfbbc994b373f
simuphil7 <simuphil7@gmail.com>
2019-11-02 18:46:53 +0530

    Created AnswerController, UserController and CommonController in quora-api

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
@@ -0,0 +1,44 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.UserDetailsResponse;
+import com.upgrad.quora.service.business.UserCommonService;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+//RestController annotation specifies that this class represents a REST API(equivalent of @Controller + @ResponseBody)
+@RestController
+@RequestMapping("/")
+public class CommonController {
+
+    //Required services are autowired to enable access to methods defined in respective Business services
+    @Autowired
+    private UserCommonService userCommonssService;
+
+    /**
+     * @param  userUuid the first {@code String} to check user profile.
+     * @param  accessToken the second {@code String} to check if the access is available.
+     * @return ResponseEntity is returned with Status OK.
+     */
+    @RequestMapping(method = RequestMethod.GET, path = "/userprofile/{userId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity < UserDetailsResponse > userProfile(@PathVariable("userId") final String userUuid, @RequestHeader("authorization") final String accessToken) throws AuthorizationFailedException, UserNotFoundException {
+        // Logic to handle Bearer <accesstoken>
+        // User can give only Access token or Bearer <accesstoken> as input.
+        String bearerToken = null;
+        try {
+            bearerToken = accessToken.split("Bearer ")[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            bearerToken = accessToken;
+        }
+        final UserEntity userEntity = userCommonssService.getUser(userUuid, bearerToken);
+        UserDetailsResponse userDetailsResponse = new UserDetailsResponse().firstName(userEntity.getFirstName())
+                .lastName(userEntity.getLastName()).userName(userEntity.getUserName()).emailAddress(userEntity.getEmail())
+                .country(userEntity.getCountry()).aboutMe(userEntity.getAboutMe()).dob(userEntity.getDob()).contactNumber(userEntity.getContactNumber());
+        return new ResponseEntity < UserDetailsResponse > (userDetailsResponse, HttpStatus.OK);
+    }
+
+}
